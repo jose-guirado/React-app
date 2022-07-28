dc4da6c19ede15c02931f06b3152cb1a32387b81
jose-guirado <arqjoseguirado@gmail.com>
2022-07-28 01:36:27 -0300

    creando la navbar para el desafio

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+// import './components/NavBar/NaVBar.css';
+
+function NaVBar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="index.html"><strong>Black Onix Diamond</strong></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Nosotros</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Tienda</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NaVBar;
